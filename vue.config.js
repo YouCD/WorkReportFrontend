7bc79e8d202dfff7a7fb18742b16d055b2366414
ycd <ycd>
2022-01-02 21:21:24 +0800

    add qiniuyun CDN

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+const VueCliPluginQiniuUploader = require("vue-cli-plugin-qiniu-uploader");
 module.exports = {
 	//避免eslint报错
-	lintOnSave:false 
+	lintOnSave:false,
+	configureWebpack: (config) => {
+		if (process.env.NODE_ENV === "production") {
+			return {
+				plugins: [new VueCliPluginQiniuUploader()],
+			};
+		}
+	},
 }
